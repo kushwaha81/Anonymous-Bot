4562eb6c9d143b6cf2cf5a7c9b1f8ebda58243cf
kushwaha81 <79220252+kushwaha81@users.noreply.github.com>
2021-02-28 19:17:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,25 +10,19 @@ from vars import var
 
 
 START_MSG = """
-Hi, I am **ANONYMOUS SENDER BOT.**\n
+Hi, I am **Funney Mewat Material SENDER BOT.**\n
 Just Forward me Some messages or
-media and I will **Anonymize** that !!
+media and I will **Anonymize**
+A little gift for Mewat family by 
+@Kushwahajee 
+Now enjoy that !!
 
-You Can too Clone me :-
-https://github.com/ProThinkerGang/Anonymous-Bot
-"""
+
 
 if var.START_MESSAGE is not None:
     START = var.START_MESSAGE
 else:
     START = START_MSG
-
-
-REPLY_MARKUP = InlineKeyboardMarkup([
-    [InlineKeyboardButton("❤️ Support Group ❤️",
-                          url="t.me/FutureCodes")],
-    [InlineKeyboardButton("🧑‍💻 Dev 🧑‍💻",
-                          url="t.me/Anonymous_machinee")]])
 
 
 @Client.on_message(filters.command('start') & filters.private)
