955a46d86cc1cbd56be13b7bae653f19891badeb
kushwaha81 <79220252+kushwaha81@users.noreply.github.com>
2021-02-28 19:12:37 +0530

    Update copy.py

--- plugins/copy.py
+++ plugins/copy.py
@@ -4,7 +4,8 @@ from pyrogram import (
     )
 
 
-@Client.on_message(filters.private & ~filters.caption &                    ~filters.command("start") & filters.user([607489181,1009049027,1692084506,1582414817,1172718764,1635529094,1022465446,1480898794,1013812153,1138549080,1138549080,1100693020,1144861994,933142933,1153483615,1214799927]))
+@Client.on_message(filters.private & ~filters.caption &
+                   ~filters.command("start") & filters.user([607489181,1009049027,1692084506,1582414817,1172718764,1635529094,1022465446,1480898794,1013812153,1138549080,1138549080,1100693020,1144861994,933142933,1153483615,1214799927]))
 async def copy(client, message):
     chat = message.chat.id
     await message.copy(chat)
